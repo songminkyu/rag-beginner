6e175a47d12208631b77ad517e9707145868c082
songminkyu <rabbircarrot@naver.com>
2025-08-11 18:29:18 +0900

    gpt-oss:20b 변경

--- src/examples/model_example/gpt_oss.py
+++ src/examples/model_example/gpt_oss.py
@@ -1,8 +1,7 @@
 from transformers import pipeline
 import torch
 
-# model_id = "openai/gpt-oss-120b"
-# model_id = "openai/gpt-oss-20b"
+model_id = "openai/gpt-oss-20b"
 
 pipe = pipeline(
     "text-generation",
